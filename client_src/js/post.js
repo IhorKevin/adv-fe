29b704d047440453184ed5b5d8a222b629387dfc
IhorKevin <ihorkevin@gmail.com>
2016-02-29 03:12:53 +0200

    add tasks: css, js, clean, html

---
+++ client_src/js/post.js
@@ -0,0 +1,26 @@
+/**
+ * Created by MOZ on 19.02.2016.
+ */
+
+$(document).ready(function(){
+
+    var post = Data.getCurrentPost();
+
+    var postTemplate = Handlebars.compile( $( '#post-page-tmpl' ).html());
+    Handlebars.registerPartial( 'post-comment', $( '#post-comment-tmpl' ).html() );
+    Handlebars.registerPartial( 'post-related', $( '#post-related-tmpl' ).html() );
+
+    function renderPost() {
+        $('.content').html(
+            postTemplate({
+                imgUrl: post.imgUrl,
+                description: post.description,
+                author: Data.getUser(post.userId).name,
+                comments: Data.getPostComments(),
+                related: Data.getRelatedPosts()
+            })
+        );
+    }
+
+    renderPost();
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,26 @@
 var gulp = require('gulp'),
     bower = require('gulp-bower'),
+    gulpif = require('gulp-if'),
+    argv = require('yargs').argv,
+
     clean = require('del'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+
+    stylus = require('gulp-stylus'),
+    axis = require('axis'),
+    prefix = require('gulp-autoprefixer'),
+
+    uglify = require('gulp-uglify'),
     imagemin = require('imagemin');
+
+var cssPath = function () {
+    return argv.prod ? 'client_build/css' : 'client_src/css';
+};
+
+var jsPath = function () {
+    return argv.prod ? 'client_build/js' : 'client_src/js';
+};
 
 gulp.task('default', ['libs', 'build']);
 gulp.task('build', ['copy-static', 'css']);
@@ -15,4 +34,38 @@ gulp.task('bower', function () {
 gulp.task('libs', function () {
     return gulp.src('client_src/libs/**/*.*')
         .pipe(gulp.dest('client_build/libs'));
+});
+
+gulp.task('html', function () {
+    gulp.src('client_src/*.html')
+        .pipe(gulp.dest('client_build'));
+});
+
+gulp.task('css', function () {
+    gulp.src('client_src/styl/main.styl')
+        .pipe(stylus({
+            use: [axis()],
+            compress: argv.prod
+        }))
+        .pipe(prefix('last 2 versions', 'ie 9'))
+        .pipe(rename('main.css'))
+        .pipe(gulpif(argv.prod, rename('main.min.css')))
+        .pipe(gulp.dest(cssPath()));
+});
+
+gulp.task('js', function () {
+    gulp.src(['client_src/js/*.js', '!main.js'])
+        .pipe(concat('main.js'))
+        .pipe(gulpif(argv.prod, uglify()))
+        .pipe(gulpif(argv.prod, rename('main.min.js')))
+        .pipe(gulp.dest(jsPath()));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('client_src/styl/*.styl', ['css']);
+    gulp.watch('client_src/js/*.js', ['js']);
+});
+
+gulp.task('clean', function () {
+    return clean('client_build');
 });
